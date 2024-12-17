4f26707e4602e5b29b4e5f78acf6f6c90d85e2ef
Paul Wooten <paul.wooten@hyland.com>
2024-12-17 14:35:07 -0500

    refactor: uuid task ids

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-
+import { v4 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];
@@ -15,7 +15,7 @@ export class TasksService {
 
     createTask(title: string, description: string) {
         const task = {
-            id: Date.now().toString(),
+            id: uuid(),
             title,
             description,
             status: TaskStatus.Open,
