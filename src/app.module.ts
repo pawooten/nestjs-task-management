8b1a155cf01b23a11bc926d57aa5da4dc96bd592
Paul Wooten <paul.wooten@hyland.com>
2025-02-13 09:28:17 -0500

    feat: add task.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TasksModule],
+    imports: [
+        TasksModule,
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: 'db.sqlite',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+    ],
 })
 export class AppModule {}
